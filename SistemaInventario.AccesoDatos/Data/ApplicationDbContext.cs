446cce08be2b3a1997140b1bf36e04a31c300c00
eliezerphp <eliezer.alvarez1993@gmail.com>
2023-09-02 22:00:55 -0600

    Configuracion del Proyecto

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
+
+namespace SistemaInventario.AccesoDatos.Data //Se agregó .AccesoDatos
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; } //DbSet del modelo bodegas, para que se pueda crear como tabla en la base de datos.
+
+        protected override void OnModelCreating(ModelBuilder builder) //Agregado para tener mejor control de las actualizaciones de las migraciones
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // hara un override de lo que actualmente existe y tomara esa configuracion  
+        }
+    }
+}
---
+++ SistemaInventario.Utilidades/DS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Utilidades
+{
+    public static class DS //Se define que será de tipo publica y estatica.
+    {
+    }
+}
